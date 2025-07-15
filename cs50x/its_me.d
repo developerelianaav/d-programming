ec2e3ef6ea1f285771b44a642f957ff40b2d5196
Elian Adolfo Argañaraz Velazco <elian@study.elian.xyz>
2025-07-15 15:46:19 -0300

    Add some work

---
+++ cs50x/its_me.d
@@ -0,0 +1,7 @@
+import std.stdio;
+
+void main() {
+	write("What's your name? ");
+	string inputName = readln();
+	write("Hello, ", inputName);
+}
---
+++ cs50x/mario_less.d
@@ -0,0 +1,16 @@
+import std.stdio;
+
+void main() {
+	char block = "#";
+	int height = 0;
+	int blocks = ;
+	int spaces = 
+
+	while (height < 1) {
+		write("Height: ");
+		readf(" %s", &height);
+	}
+
+	for () {
+	}
+}
